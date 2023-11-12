68ea01310b26808cfee3379749a09f5cb62acabe
kangvcar <kangvcar@126.com>
2023-11-12 17:16:56 +0800

    Title: Add main.spec and update index.html and main.py
    Body:
    A new file, main.spec, has been added to enhance the application's specification. This addition will improve the overall functionality of the application. The index.html and main.py files have also been modified to accommodate this new feature. These changes were necessary to ensure the application remains efficient and user-friendly.

--- gui/main.py
+++ gui/main.py
@@ -7,6 +7,12 @@ import logging
 from apple_id_checker import AppleIDChecker
 from threading import Lock, Event
 from concurrent.futures import ThreadPoolExecutor
+import tempfile
+import os
+
+
+result_file_path = None
+
 # 配置日志记录
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -19,7 +25,8 @@ NUM_PROXIES_TO_FETCH = 200  # 每次API调用最多获取的代理数
 proxy_queue = queue.Queue()
 proxy_queue_lock = Lock()  # A lock to ensure thread-safe operations on the proxy queue
 stop_event = Event()  # An event to signal the threads to stop
-
+# 全局锁用于文件写入
+write_results_lock = threading.Lock()
 
 def fetch_and_replenish_proxy_queue_thread_safe(num_proxies):
     with proxy_queue_lock:  # Ensure that only one thread can replenish the queue at a time
@@ -36,6 +43,17 @@ def fetch_and_replenish_proxy_queue(num_proxies):
             proxy_queue.put(proxy)
 
 
+def write_result_to_file(apple_id, password, status):
+    global result_file_path
+    if result_file_path is None:
+        temp_file = tempfile.NamedTemporaryFile(delete=False, mode='w', suffix='.txt', prefix='results_', dir='.')  # 创建临时文件
+        result_file_path = temp_file.name
+        temp_file.close()   # 创建文件后立即关闭，稍后再次打开进行写入
+
+    with write_results_lock, open(result_file_path, 'a') as file:  # 使用锁保证线程安全
+        file.write(f"{apple_id}----{password}---{status}\n")
+
+
 # 使用提供的代理检查Apple ID的函数
 def check_apple_id(apple_id, password):
     # Assuming the AppleIDChecker class and its try_login method are correctly defined
@@ -50,7 +68,9 @@ def check_apple_id(apple_id, password):
 
             proxy = proxy_queue.get(timeout=5)  # Get a proxy with a timeout
             result = checker.try_login(apple_id, password, proxy)
-            return result  # This will exit only the current thread
+            write_result_to_file(apple_id, password, result['status'])  # 实时写入结果
+            return {'apple_id': apple_id, 'password': password, 'status': result['status']}
+            # return result  # This will exit only the current thread
         except queue.Empty:
             logger.error(
                 "Queue is empty, no available proxy IPs, stopping check.")
@@ -102,6 +122,39 @@ class Api:
         for proxy in proxies:
             proxy_queue.put(proxy)
 
+    def get_results_file_path(self):
+        global result_file_path
+        return result_file_path if result_file_path and os.path.exists(result_file_path) else ''
+
+    # 在 Api 类中修改 download_file 方法
+    def download_file(self):
+        global result_file_path
+        try:
+            # Ensure the result file exists before trying to open a save dialog
+            if result_file_path and os.path.exists(result_file_path):
+                # Run the save dialog in the main thread
+                save_dialog = webview.windows[0].create_file_dialog(webview.SAVE_DIALOG, directory=os.path.dirname(result_file_path), save_filename=os.path.basename(result_file_path))
+                if save_dialog:
+                    # Only attempt to write to the file if a save path was provided
+                    with open(result_file_path, 'r') as source, open(save_dialog, 'w') as target:
+                        target.write(source.read())
+                    return 'File saved successfully to ' + save_dialog
+                else:
+                    return 'File save cancelled by the user.'
+            else:
+                return 'No result file found. Please run the checks first.'
+        except Exception as e:
+            logger.exception("Failed to save the file.")
+            return f"An error occurred while saving the file: {str(e)}"
+
+    def get_results_file(self):
+        # This path must be accessible by the web server serving the files
+        return './results.txt'
+
+    def stop_checking(self):
+        logger.info("Stopping all threads...")
+        stop_event.set()  # Signal all threads to stop
+
     # 开始检查Apple IDs的对外暴露方法
     def check_apple_ids(self, file_content, num_threads):
         if self.is_checking:  # 如果当前正在检查，则返回
@@ -114,9 +167,10 @@ class Api:
         apple_ids = [line.split(
             '----') for line in file_content.strip().split('\n') if '----' in line]
 
+        # detail_results = []
         # 使用线程池执行并发检查
         results = {"correct": 0, "incorrect": 0, "locked": 0, "exception": 0,
-                   "total": len(apple_ids), "detected": 0, "undetected": 0}
+                    "total": len(apple_ids), "detected": 0, "undetected": 0}
         with ThreadPoolExecutor(max_workers=20) as executor:
             futures_to_apple_id = {executor.submit(
                 check_apple_id, apple_id, password): apple_id for apple_id, password in apple_ids}
@@ -145,21 +199,16 @@ class Api:
 
                     # 使用线程安全的方式更新UI
                     update_ui_with_results(results)
+                    # detail_results.append(result)
                 except Exception as exc:
                     logger.error(
                         f'Apple ID check for {apple_id} generated an exception: {exc}')
+            # self.write_result_to_file(detail_results)
 
         self.is_checking = False  # 重置检查标志
 
-# 安全地在主线程中更新UI的函数
-
-
-def stop_checking():
-    stop_event.set()  # Signal all threads to stop
 
 # 安全地在主线程中更新UI的函数
-
-
 def update_ui_with_results(results):
     # 构造更新UI的JavaScript代码
     update_ui_script = f"""
@@ -181,7 +230,7 @@ def create_webview_window():
     global global_window  # Use the global keyword to modify the global variable
     api = Api()
     global_window = webview.create_window(
-        'Apple ID Checker', 'index.html', js_api=api)
+        'Apple ID Checker', 'index.html', js_api=api, width=800, height=500, resizable=False, min_size=(800, 500))
     webview.start()
 
 
